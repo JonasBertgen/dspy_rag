281881c813157163d9b0c3a1fc275eb3ca43f792
JonasBertgen <jonas.bertgen@gmail.com>
2025-10-26 18:59:43 +0100

    store the embeddings, and only load them with request

--- ingestion.py
+++ ingestion.py
@@ -1,13 +1,17 @@
 import PyPDF2
 from loguru import logger
+import chromadb
+from chromadb.utils import embedding_functions
 
 
-def read_pdf_file(file_path: str):
-    # for now each page as chunck
+colbert_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name="colbert-ir/colbertv2.0")
+
+
+def read_pdf_file(file_path: str) -> list[str]:
 
     with open(file_path, 'rb') as file:
         pdf_data = []
-        print(pdf_data)
         pdf_reader = PyPDF2.PdfReader(file)
         for page_number in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_number]
@@ -16,12 +20,32 @@ def read_pdf_file(file_path: str):
 
         logger.info(f"read file at {file_path} with char length {
             len(pdf_data)}.")
+        # logger.info(pdf_data[15])
         return pdf_data
 
 
-def main():
+def create_chroma_db(chunks: list[str]):
+    chroma_client = chromadb.PersistentClient("data/chroma_db")
 
-    read_pdf_file("data/profilicity.pdf")
+    collection = chroma_client.create_collection(
+        name="rag_collection",
+        embedding_function=colbert_ef
+    )
+
+    documents = read_pdf_file("data/profilicity.pdf")
+    collection.add(documents=documents, ids=[
+                   str(i) for i in range(len(documents))])
+
+
+def get_chroma_db(path: str):
+    client = chromadb.PersistentClient("data/chroma_db")
+    collection = client.get_collection(
+        name="rag_collection",
+        embedding_function=colbert_ef)
+    return collection
+
+
+def main():
 
 
 if __name__ == "__main__":
--- rag.py
+++ rag.py
@@ -1,28 +1,8 @@
 import dspy
-import chromadb
-from chromadb.utils import embedding_functions
-from ingestion import read_pdf_file
+from loguru import logger
+from ingestion import get_chroma_db
 
 # todo move to ingestion.py
-chroma_client = chromadb.Client()
-
-
-colbert_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="colbert-ir/colbertv2.0")
-
-collection = chroma_client.create_collection(
-    name="rag_collection",
-    embedding_function=colbert_ef
-)
-
-documents = ["Python is a high-level programming language known for its simplicity and readability.",
-             "Machine learning is a subset of artificial intelligence that enables systems to learn from data.",
-             "DSPy is a framework for programming with foundation models using declarative modules.",
-             "ChromaDB is an open-source embedding database designed for AI applications.",
-             "ColBERT is a neural retrieval model that uses contextualized late interaction over BERT."]
-
-collection.add(documents=documents, ids=[str(i)
-               for i in range(len(documents))])
 
 
 class ChromaRetriever(dspy.Retrieve):
@@ -32,7 +12,8 @@ class ChromaRetriever(dspy.Retrieve):
 
     def forward(self, query: str) -> list[str]:
         results = self.collection.query(query_texts=[query], n_results=self.k)
-        return results['documents'][0]
+        logger.info(results['documents'])
+        return results['documents']
 
 
 class RAG(dspy.Signature):
@@ -48,12 +29,10 @@ class SimpleRAG(dspy.Module):
     def __init__(self, retriever):
         super().__init__()
         self.retriever = retriever
-        self.generator = dspy.ChainOfThought(RAG)
+        self.generator = dspy.Predict(RAG)
 
     def forward(self, question):
-        # Retrieve relevant documents
         context = self.retriever(question)
-        # Generate answer using retrieved context
         response = self.generator(context=context, question=question)
         return response
 
@@ -62,8 +41,10 @@ lm = dspy.LM("ollama_chat/mistral",
              api_base="http://localhost:11434", api_key="")
 dspy.configure(lm=lm)
 
-retriever = ChromaRetriever(collection, k=2)
+
+collection = get_chroma_db("data/chroma_db")
+retriever = ChromaRetriever(collection, k=3)
 rag = SimpleRAG(retriever)
 
-result = rag("what is dspy?")
-print(result)
+result = rag("Is Donald Trump authentic?")
+print(result["answer"].strip())
